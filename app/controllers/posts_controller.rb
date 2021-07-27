59791e6d3ba4a5630f7f7fd03e809f1b88fc9742
Marcin Doliwa <mdoliwa@gmail.com>
2021-07-27 21:53:46 +0200

    Show points count

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,8 @@ class PostsController < ApplicationController
   include Pagy::Backend
 
   def index
-    @posts = Post.upvoted_by(current_user).includes(:website).ranked
+    @posts = current_user ? Post.upvoted_by(current_user) : Post.all
+    @posts = @posts.includes(:website).ranked
     @posts = @posts.where(website: website) if params[:website_id]
 
     @pagy, @posts = pagy(@posts)
