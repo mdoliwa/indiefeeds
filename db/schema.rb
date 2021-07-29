a98c7d1fd850dd653c5864a228a06c6019c8b8e9
Marcin Doliwa <mdoliwa@gmail.com>
2021-07-29 22:17:25 +0200

    Add comments count to post

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_27_205156) do
+ActiveRecord::Schema.define(version: 2021_07_29_201542) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 2021_07_27_205156) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "upvotes_count", default: 0
+    t.integer "comments_count", default: 0
     t.index ["website_id"], name: "index_posts_on_website_id"
   end
 
