354cea926d12957f20f4ca85e1e16395b436225e
Marcin Doliwa <mdoliwa@gmail.com>
2021-10-08 22:52:41 +0200

    Make feed dependent destroy on website

--- app/models/website.rb
+++ app/models/website.rb
@@ -1,6 +1,6 @@
 class Website < ApplicationRecord
   has_many :posts, dependent: :destroy
-  has_one :feed
+  has_one :feed, dependent: :destroy
 
   def website_host
     URI.parse(url).host.downcase
