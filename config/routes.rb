1ca7fd6e2db5d7a9bb8b55088f1c8a20b1465a46
Marcin Doliwa <mdoliwa@gmail.com>
2021-10-11 21:47:30 +0200

    Add sidekiq web ui

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
+require 'sidekiq/web'
+
 Rails.application.routes.draw do
   devise_for :users
+
+  authenticate :user, lambda { |u| u.admin? } do
+    mount Sidekiq::Web => '/sidekiq'
+  end
 
   root to: 'posts#index'
 
