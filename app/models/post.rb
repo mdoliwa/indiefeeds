5c6c5debc8b26a8946ee703e4d7bfff44352638f
Marcin Doliwa <mdoliwa@gmail.com>
2021-10-17 21:44:52 +0200

    Show only published posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,5 +5,5 @@ class Post < ApplicationRecord
 
   scope :upvoted_by, -> (user) { joins("LEFT OUTER JOIN upvotes ON posts.id = upvotes.upvotable_id AND upvotes.upvotable_type = 'Post' AND upvotes.user_id = #{user.id}").select('posts.*, (upvotes.id IS NOT NULL) AS upvoted') if user.present? }
 
-  scope :ranked, -> { select('posts.*, ((posts.upvotes_count + 1) / POWER(EXTRACT(EPOCH FROM (NOW() - posts.published_at)), 1.8)) AS rank').order('rank DESC') }
+  scope :ranked, -> { select('posts.*, ((posts.upvotes_count + 1) / POWER(EXTRACT(EPOCH FROM (NOW() - posts.published_at)), 1.8)) AS rank').where('published_at < ?', Time.zone.now).order('rank DESC') }
 end
